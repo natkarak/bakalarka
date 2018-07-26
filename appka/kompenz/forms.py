1bd071a485b890e2cac6d96eef80fcb8f2414e5d
Natalia Rakovska <natka.rak@gmail.com>
2018-07-26 13:36:11 +0200

    nieco

--- appka/kompenz/forms.py
+++ appka/kompenz/forms.py
@@ -6,7 +6,16 @@ class  KontaktForm(forms.Form):
 	email = forms.EmailField(required = True)
 	sprava = forms.CharField(required = True, widget = forms.Textarea)
 
+OTAZKA0 = (
+	('opt1','áno - poznám svoju mieru postihu'),
+	('opt2','nie - chcem ju zistiť podľa diagnózy'),
+	)
 
+class Otazka0Form(forms.Form):
+	Chcete_zadať_mieru_postihu = forms.ChoiceField(choices = OTAZKA0, widget = forms.RadioSelect)
+
+
+		
 OTAZKA1 = (
 	('a', 'a.	Nervový systém (poškodenie mozgu, miechy a periférnych nervov'),
 	('b', 'b.	Pohybový a podporný aparát'),
@@ -14,9 +23,6 @@ OTAZKA1 = (
 
 class Otazka1Form(forms.Form):
 	Aké_máte_postihnutie = forms.ChoiceField(choices = OTAZKA1, widget = forms.RadioSelect)
-	
-		
-
 
 
 
--- appka/kompenz/views.py
+++ appka/kompenz/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import Otazka1Form, KontaktForm
+from .forms import Otazka0Form, Otazka1Form, KontaktForm
 
 app_name= 'kompenz'
 
@@ -11,8 +11,20 @@ def index(request):
 
 
 def dotaznik(request):
-	
-	return render(request, 'kompenz/dotaznik.html')
+	context = {'otazka0Form' : Otazka0Form()}
+	if request.method == 'GET': 
+		form = Otazka0Form(request.GET)
+		zad_postih = request.GET.get('zad_postih')
+		if 'zad_postih' == 'opt1':
+			return HttpResponseRedirect('kompenz/otazka1.html')
+		elif 'zad_postih' == 'opt2':
+			return HttpResponseRedirect('kompenz/kontakty.html')
+	else:
+		form = Otazka0Form()
+	#context['otazka0Form'] = form
+	#form_class = Otazka0Form
+	#Chcete_zadať_mieru_postihu = data.get('OTAYZKA0')
+	return render(request, 'kompenz/dotaznik.html', {'form':form})
 
 def odkazy(request):
 	
