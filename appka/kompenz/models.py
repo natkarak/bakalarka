411922576a098d806280c57a91a1a6ef3d194fe6
Natalia Rakovska <natka.rak@gmail.com>
2018-08-17 03:30:32 +0200

    model uprava

--- appka/kompenz/models.py
+++ appka/kompenz/models.py
@@ -10,19 +10,6 @@ class Postihnutie (models.Model):
 	spodnaHranica =models.IntegerField(default = 0) # miera postihnutia v %
 	hornaHranica= models.IntegerField(default = 100)
 
-	NERVOVE = 'NS'
-	POHYBOVE = 'PH'
-
-	KATEGORIA_POSTIH = (
-		(NERVOVE, 'Nervový systém - poškodenie mozgu, miechy a periférnych nervov'),
-		(POHYBOVE, 'Pohybový a podporný systém')
-	)
-
-	kategoria_postih = models.CharField( max_length = 2, choices = KATEGORIA_POSTIH)
-	
-	def is_upperclass(self):
-		return self.kategoria_postih in (self.NERVOVE, self.POHYBOVE)
-
 	def __str__(self):
 		"""A function inside the class Postihnutie, to show the nazov"""
 		return self.nazov
@@ -32,7 +19,10 @@ class Postihnutie (models.Model):
 class Kompenzacia(models.Model):
 	nazov = models.CharField(max_length = 100)
 	oblastKomp = models.CharField(max_length = 200)
-
+	dochodkovyVek = models.BooleanField(default = False)
+	jednorazova = models.BooleanField()
+	maxPrispevok = models.IntegerField(default = 100000)
+	prijem = models.IntegerField(default = 350)
 
 	def __str__(self):
 		"""A function inside the class Postihnutie, to show the nazov"""
